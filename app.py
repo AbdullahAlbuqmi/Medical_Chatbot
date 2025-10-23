864b2a925b3e7bda8b0dada51df5ee26a35d8533
Abdullah <albuqmi49@outlook.com>
2025-10-23 03:54:44 +0300

    new commit

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ except Exception:
 
 # ---------- config & logging ----------
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(_name_)
+logger = logging.getLogger(__name__)
 
 # ---------- FastAPI Init ----------
 app = FastAPI(title="Medical AI Chatbots API")
